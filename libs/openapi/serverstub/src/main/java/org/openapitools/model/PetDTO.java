1eb18e10528ee4213bf9338764dcba0996a43e47
torsten <no@no.de>
2024-07-02 18:21:21 +0300

    copy generated code to serverstub project

---
+++ libs/openapi/serverstub/src/main/java/org/openapitools/model/PetDTO.java
@@ -0,0 +1,148 @@
+package org.openapitools.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * PetDTO
+ */
+
+@JsonTypeName("Pet")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", comments = "Generator version: 7.6.0")
+public class PetDTO implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private Long id;
+
+  private String name;
+
+  private String tag;
+
+  public PetDTO() {
+    super();
+  }
+
+  /**
+   * Constructor with only required parameters
+   */
+  public PetDTO(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public PetDTO id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+  */
+  @NotNull 
+  @Schema(name = "id", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("id")
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public PetDTO name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+  */
+  @NotNull 
+  @Schema(name = "name", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("name")
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public PetDTO tag(String tag) {
+    this.tag = tag;
+    return this;
+  }
+
+  /**
+   * Get tag
+   * @return tag
+  */
+  
+  @Schema(name = "tag", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("tag")
+  public String getTag() {
+    return tag;
+  }
+
+  public void setTag(String tag) {
+    this.tag = tag;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PetDTO pet = (PetDTO) o;
+    return Objects.equals(this.id, pet.id) &&
+        Objects.equals(this.name, pet.name) &&
+        Objects.equals(this.tag, pet.tag);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, tag);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class PetDTO {\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    tag: ").append(toIndentedString(tag)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
