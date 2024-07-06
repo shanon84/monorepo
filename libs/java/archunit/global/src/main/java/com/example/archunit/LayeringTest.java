f872e18704cad29b9f418aa1dbca6c5ba68bcedd
shanon84 <tottinator@gmail.com>
2024-07-06 23:00:43 +0200

    add layering test. Support for archunit. changed remote cache ip to ipv4

--- libs/java/archunit/global/src/main/java/com/example/archunit/LayeringTest.java
+++ libs/java/archunit/global/src/main/java/com/example/archunit/LayeringTest.java
@@ -1,4 +1,47 @@
 package com.example.archunit;
 
+import com.example.global.annotations.Persistence;
+import com.example.global.annotations.Properties;
+import com.example.global.annotations.Scheduler;
+import com.tngtech.archunit.core.domain.properties.CanBeAnnotated;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.library.Architectures;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+@AnalyzeClasses(packages = "*", importOptions = {ImportOption.DoNotIncludeArchives.class, ImportOption.DoNotIncludeJars.class, ImportOption.DoNotIncludeTests.class})
 public class LayeringTest {
+
+  public static final String CONTROLLERS = "Controllers";
+  public static final String SCHEDULERS = "Schedulers";
+  public static final String CONFIGURATIONS = "Configurations";
+  public static final String PROPERTIES = "Properties";
+  public static final String SERVICES = "Services";
+  public static final String PERSISTENCES = "Persistences";
+  public static final String REPOSITORIES = "Repositories";
+  
+  @ArchTest
+  static final ArchRule layer_dependencies_are_respected = Architectures.layeredArchitecture().consideringAllDependencies()
+
+    .layer(CONFIGURATIONS).definedBy(CanBeAnnotated.Predicates.annotatedWith(Configuration.class))
+    .layer(CONTROLLERS).definedBy(CanBeAnnotated.Predicates.annotatedWith(Controller.class).or(CanBeAnnotated.Predicates.annotatedWith(RestController.class)))
+    .layer(SCHEDULERS).definedBy(CanBeAnnotated.Predicates.annotatedWith(Scheduler.class))
+    .layer(PROPERTIES).definedBy(CanBeAnnotated.Predicates.annotatedWith(Properties.class))
+    .layer(SERVICES).definedBy(CanBeAnnotated.Predicates.annotatedWith(Service.class))
+    .layer(PERSISTENCES).definedBy(CanBeAnnotated.Predicates.annotatedWith(Persistence.class))
+    .layer(REPOSITORIES).definedBy(CanBeAnnotated.Predicates.annotatedWith(Repository.class))
+
+    .whereLayer(CONFIGURATIONS).mayNotBeAccessedByAnyLayer()
+    .whereLayer(CONTROLLERS).mayOnlyBeAccessedByLayers(CONFIGURATIONS)
+    .whereLayer(SCHEDULERS).mayOnlyBeAccessedByLayers(CONFIGURATIONS)
+    .whereLayer(SERVICES).mayOnlyBeAccessedByLayers(CONTROLLERS, SCHEDULERS, CONFIGURATIONS)
+    .whereLayer(PERSISTENCES).mayOnlyBeAccessedByLayers(SERVICES)
+    .whereLayer(REPOSITORIES).mayOnlyBeAccessedByLayers(PERSISTENCES);
 }
+
